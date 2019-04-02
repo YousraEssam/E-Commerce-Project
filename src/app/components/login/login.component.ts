52d6cbb83d8933f4ec1680bdc6940afd2ab6e687
YousraEssam <yousraa.essam.15@hotmail.com>
2019-04-02 18:22:07 +0200

    login form added

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl} from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+  loginForm = new FormGroup({
+    email : new FormControl(''),
+    password : new FormControl(''),
+  });
+
+  onSubmit() {
+    console.warn(this.loginForm.value);
+  }
   constructor() { }
 
   ngOnInit() {
