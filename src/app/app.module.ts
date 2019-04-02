7755a2f307e110fb016df1ce35b7aebb61f08f8e
YousraEssam <yousraa.essam.15@hotmail.com>
2019-04-02 15:24:26 +0200

    creating components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { CartCounterComponent } from './components/cart-counter/cart-counter.component';
+import { WishListComponent } from './components/wish-list/wish-list.component';
+import { SingleProductComponent } from './components/single-product/single-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    CartCounterComponent,
+    WishListComponent,
+    SingleProductComponent
   ],
   imports: [
     BrowserModule,
