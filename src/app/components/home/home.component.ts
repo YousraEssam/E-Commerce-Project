08ac9d6caf9c6a27912718f88823c86266bb110d
YousraEssam <yousraa.essam.15@hotmail.com>
2019-04-05 19:31:11 +0200

    render products in home page from json object

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
@@ -8,12 +9,19 @@ import { Router } from '@angular/router';
 })
 export class HomeComponent implements OnInit {
 
-  constructor(private router: Router) { }
+  products: object[];
+
+  constructor(private router: Router, private http: HttpClient) { }
   
   public navigateTo(path: string): void {
     this.router.navigate([path]);
   }
+
   ngOnInit() {
+    this.http.get('../../../assets/products_list.json')
+        .subscribe(data => {
+        this.products = data["productsList"];
+      });
   }
 
 }
