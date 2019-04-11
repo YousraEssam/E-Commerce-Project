5e7476dd6b9fa08de7cf7fa38338f5232f1429e6
YousraEssam <yousraa.essam.15@hotmail.com>
2019-04-11 13:37:34 +0200

    add product model

---
+++ src/app/models/products.model.ts
@@ -0,0 +1,11 @@
+export class ProductsModel {
+    id: number
+    img: string
+    name: string
+    priceBeforeSale: number
+    priceAfterSale: number
+    category: string
+    quantity: number
+    rating: number
+    desc: string
+}
