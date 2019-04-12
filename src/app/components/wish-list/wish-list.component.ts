c60d86de10c126d680392a79db645b05f017d652
YousraEssam <yousraa.essam.15@hotmail.com>
2019-04-12 16:18:02 +0200

    render products in wish list page

--- src/app/components/wish-list/wish-list.component.ts
+++ src/app/components/wish-list/wish-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-wish-list',
@@ -7,9 +9,22 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WishListComponent implements OnInit {
 
-  constructor() { }
+  logged: any;
+  wish: any;
+  products: object[];
+  id: number;
+  wishListContent = JSON.parse(localStorage.getItem("wishList")) ? JSON.parse(localStorage.getItem("wishList")) : [];
+
+  constructor(private router: Router, private http: HttpClient, private route :ActivatedRoute) { }
 
   ngOnInit() {
+    this.logged = JSON.parse(localStorage.getItem('LoggedUsers'));
+    this.wish = JSON.parse(localStorage.getItem('wishList'));
+    this.http.get('../../../assets/products_list.json')
+    .subscribe(data => {
+    this.products = data["productsList"];
+  });
+  this.id = parseInt(this.route.snapshot.paramMap.get('id'));
   }
 
 }
